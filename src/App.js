84c7ffcebe5924b3cf75449a62af1e0de04220be
Jose Machado <jgbm81@hotmail.com>
2023-10-30 16:46:18 -0400

    experience grid

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import './App.scss';
 import { Fragment } from 'react';
 import './styles/home.scss';
 import Experience from './components/experience/experience';
-import Information from './components/about-me/information';
 import EmailIcon from './components/icons/email-icon';
 import SocialBtns from './components/social-btns/social-btns';
 import NavigationBar from './components/nav-bar/nav-bar';
 import Tecnologies from './components/tecnologies/tecnologies';
 import Projects from './components/projects-list/projects';
+import { Grid } from '@mui/material';
 
 
 
@@ -17,11 +17,18 @@ function App() {
     <Fragment>
       <NavigationBar />
       <main className='container'>
+        <Grid container spacing={2} >
+          <Grid item xs={12} display='flex' justifyContent='center' >
+            <Experience />
+          </Grid>
+          <Grid item xs={12}>
+            <Tecnologies />
+          </Grid>
 
-        <Experience />
-        <Tecnologies />
-        
-        <Projects />
+
+          <Projects />
+        </Grid>
+
 
 
         <div className='buttons'>
--- src/components/experience/experience.jsx
+++ src/components/experience/experience.jsx
@@ -1,21 +1,25 @@
-
 import './experience.styles.scss';
 import SectionTitle from '../sectionTitle/sectionTitle';
 import { Image } from 'react-bootstrap';
 import MinhaFoto from '../../assets/me.png';
+import { Grid } from '@mui/material';
+
+
 const Experience = () => {
 
     return (
         <div className='experience'>
-            <Image src={MinhaFoto} width={325} height={277} />
-            <div className='description'>
-                <SectionTitle text='Desenvolvedor Front-end' />
-                <p>Sou um Engenheiro de Software com um desejo insaciável de crescimento e inovação. Com uma sólida base em tecnologias web e um olhar aguçado para detalhes, estou pronto para mergulhar de cabeça em projetos desafiadores que me permitirão crescer ainda mais no mundo da engenharia de software. </p>
-
-            </div>
-
-
-
+            <Grid container spacing={2} justifyContent='center'>
+                <Grid item xs={12} sm={5}>
+                    <Image src={MinhaFoto} width={325} height={277} />
+                </Grid>
+                <Grid item xs={12} sm={7}>
+                    <div className='description'>
+                        <SectionTitle text='Desenvolvedor Front-end' />
+                        <p>Sou um Engenheiro de Software com um desejo insaciável de crescimento e inovação. Com uma sólida base em tecnologias web e um olhar aguçado para detalhes, estou pronto para mergulhar de cabeça em projetos desafiadores que me permitirão crescer ainda mais no mundo da engenharia de software. </p>
+                    </div>
+                </Grid>
+            </Grid>
         </div>
     )
 }
